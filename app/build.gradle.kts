dec8094671982e0e46087f0fa65d29a96a1075fd
Mohammed Zaid <mohammedzaid00100@gmail.com>
2026-09-17 15:50:40 +0530

    Bump Aura Music to 1.1.4 for playback notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.auramusic.kznrpx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.1.1"
+    versionCode = 6
+    versionName = "1.1.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
