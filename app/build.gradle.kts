d9cf784c91843cd98e731e5161fd34f5f3755161
Mohammed Zaid <mohammedzaid00100@gmail.com>
2026-09-18 01:18:59 +0530

    Release Aura 1.1.6 crash and launcher icon fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.auramusic.kznrpx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 7
-    versionName = "1.1.5"
+    versionCode = 8
+    versionName = "1.1.6"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
